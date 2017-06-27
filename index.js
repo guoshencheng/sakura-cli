77f9c6d25a2bd748ea6f9a87ab0ef24f363a5e02
guoshencheng <648772021@qq.com>
2017-06-27 16:57:29 +0800

    add scripts

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var program = require('commander');
-var packageJson = require('./package.json');
-var chalk = require('chalk');
+const packageJson = require('./package.json');
+const chalk = require('chalk');
+const lib = require('./lib');
 program.version(packageJson.version)
-.option("-r --roadhog")
-.option("-n --normal")
+.command("create <name>").action(function(name) {
+  console.log(chalk.yellow("CREATE => TODO add create command handler, write code here"), {
+    name
+  });
+  lib.craete({ name });
+  //TODO: add create command handler
+})
+program.option("-n --normal")
 .command("init").action(function() {
   console.log(chalk.yellow("INIT => TODO add init command handler, write code here"), {
     roadhog: program.roadhog,
     normal: program.normal
   });
+  lib.init({})
   //TODO: add init command handler
 })
-.command("publish").action(function() {
-
+program.command("publish").action(function() {
+  lib.publish()
 })
+
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import craete from './create.js';
+import init from './init.js';
+import publish from './publish.js';
 
+module.exports = {
+  craete, init, publish
+}
