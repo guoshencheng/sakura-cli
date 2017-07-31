b32ec4173e394aa541c968dcf42530c24de8dc41
guoshencheng <648772021@qq.com>
2017-07-31 10:55:47 +0800

    sakura publsh

--- src/publish.js
+++ src/publish.js
@@ -4,11 +4,17 @@ import axios from 'axios';
 const pwd = process.cwd();
 import chalk from 'chalk';
 
+const env = process.env.NODE_ENV;
+
 module.exports = () => {
   var sakuraConfig;
   var resources;
   try {
     sakuraConfig = require(path.resolve(pwd, './sakura.config.json'));
+    if (sakuraConfig[env]) {
+      sakuraConfig = Object.assign(sakuraConfig, sakuraConfig[env])
+    }
+    console.log(chalk.green("Read config success → " + JSON.stringify(sakuraConfig)))
   } catch (e) {
     console.log(chalk.red("Error → without sakura config file"))
     process.exit(1);
@@ -18,9 +24,10 @@ module.exports = () => {
   } catch (e) { /* handle error */ }
   const appid = sakuraConfig.appid;
   var sakuraServer = sakuraConfig.sakuraServer;
-  axios.post(sakuraServer + `/api/apps/${appid}/all`, {
-    config: sakuraConfig.config,
-    resources
+  axios.post(sakuraServer + `/api/v1/webapps/${appid}/resources`, {
+    javascripts: resources.javascripts.join(','),
+    styles: resources.styles.join(','),
+    version: resources.hash
   }).then(response => {
     if (response.status == 200 && response.data) {
       console.log(chalk.green("→ success upload"))
@@ -28,6 +35,6 @@ module.exports = () => {
       console.log(chalk.red("→ fail upload with status: " + response.status))
     }
   }).catch(error => {
-    console.log(chalk.red("→ fail upload with code: " + error.code))
+    console.log(chalk.red("→ fail upload"))
   })
 }
